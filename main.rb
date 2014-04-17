603a1e65583f769a5307d0c65a574b9dab5cc566
Todd Lunter <tlunter@gmail.com>
2014-04-17 00:18:59 -0400

    Better ordering/weird indent?

--- main.rb
+++ main.rb
@@ -9,7 +9,7 @@ def get_page(url)
 end
 
 def find_direction(json, bad_locations)
-  ["east", "south", "north", "west"].each do |dir|
+  ["north", "east", "south", "west"].each do |dir|
     if json["goal"].include?(dir) && json.has_key?(dir) && !bad_locations.include?(json[dir])
       return json[dir]
     end
@@ -19,7 +19,7 @@ def find_direction(json, bad_locations)
 end
 
 def pick_from_other_directions(json, bad_locations)
-  ["east", "south", "north", "west"].each do |dir|
+  ["north", "east", "south", "west"].each do |dir|
     if json.has_key?(dir) && !bad_locations.include?(json[dir])
       return json[dir]
     end
@@ -28,42 +28,42 @@ def pick_from_other_directions(json, bad_locations)
 end
 
 def drive_page(starting_point)
-    bad_locations = {}
-    previous_location = []
-    current_location = starting_point
-    iteration = 1
-    loop do
-      puts "Iteration #{iteration}"
-      bad_locations[current_location] ||= []
-      begin
-        json = get_page(current_location)
-      rescue
-        binding.pry
-      end
-      if !json.has_key?("goal") || !(json["goal"].include?("lion") || json["goal"].include?("north") || json["goal"].include?("east"))
-        puts json
-        break
-      end
-      new_location = find_direction(json, bad_locations[current_location])
+  bad_locations = {}
+  previous_location = []
+  current_location = starting_point
+  iteration = 1
+  loop do
+    puts "Iteration #{iteration}"
+    bad_locations[current_location] ||= []
+    begin
+      json = get_page(current_location)
+    rescue
+      binding.pry
+    end
+    if !json.has_key?("goal") || !(json["goal"].include?("lion") || json["goal"].include?("north") || json["goal"].include?("east"))
+      puts json
+      break
+    end
+    new_location = find_direction(json, bad_locations[current_location])
 
-      if new_location.nil?
-        new_location = pick_from_other_directions(json, bad_locations[current_location])
-        if new_location.nil? || new_location == starting_point
-          bad_locations[previous_location.last].push(current_location)
-          current_location = previous_location.pop
-        elsif previous_location.include?(new_location)
-          puts "Cyclical?" if previous_location.last != new_location
-          bad_locations[current_location].push(new_location)
-        else
-          previous_location.push(current_location)
-          current_location = new_location
-        end
+    if new_location.nil?
+      new_location = pick_from_other_directions(json, bad_locations[current_location])
+      if new_location.nil? || new_location == starting_point
+        bad_locations[previous_location.last].push(current_location)
+        current_location = previous_location.pop
+      elsif previous_location.include?(new_location)
+        puts "Cyclical?" if previous_location.last != new_location
+        bad_locations[current_location].push(new_location)
       else
         previous_location.push(current_location)
         current_location = new_location
       end
-      iteration += 1
+    else
+      previous_location.push(current_location)
+      current_location = new_location
     end
+    iteration += 1
+  end
 end
 
 drive_page('http://jobs.happier.com/1111')
