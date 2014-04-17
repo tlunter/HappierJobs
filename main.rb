2c6809766a30368e4f5acbbd5cdcc1ff4c9febc0
Todd Lunter <tlunter@gmail.com>
2014-04-17 00:11:47 -0400

    A still incomplete version

--- main.rb
+++ main.rb
@@ -1,24 +1,69 @@
 require 'excon'
 require 'json'
+require 'pry'
 
 def get_page(url)
   res = Excon.get(url)
-  json = JSON.parse(res.body)
-
-  puts json["goal"]
-
-  get_direction(json)
+  JSON.parse(res.body).tap do |json|
+  end
 end
 
-def get_direction(json)
+def find_direction(json, bad_locations)
   ["east", "south", "north", "west"].each do |dir|
-    if json["goal"].include?(dir) && json.has_key?(dir)
-      puts "Getting #{json[dir]}"
-      get_page(json[dir])
-      return
+    if json["goal"].include?(dir) && json.has_key?(dir) && !bad_locations.include?(json[dir])
+      return json[dir]
     end
   end
-  puts "What now?"
+
+  nil
 end
 
-get_page('http://jobs.happier.com/1111')
+def pick_from_other_directions(json, bad_locations)
+  ["east", "south", "north", "west"].each do |dir|
+    if json.has_key?(dir) && !bad_locations.include?(json[dir])
+      return json[dir]
+    end
+  end
+  nil
+end
+
+def drive_page(starting_point)
+    bad_locations = {}
+    previous_location = []
+    current_location = starting_point
+    iteration = 1
+    loop do
+      puts "Iteration #{iteration}"
+      bad_locations[current_location] ||= []
+      begin
+        json = get_page(current_location)
+      rescue
+        binding.pry
+      end
+      if !json.has_key?("goal") || !(json["goal"].include?("lion") || json["goal"].include?("north") || json["goal"].include?("east"))
+        puts json
+        break
+      end
+      new_location = find_direction(json, bad_locations[current_location])
+
+      if new_location.nil?
+        new_location = pick_from_other_directions(json, bad_locations[current_location])
+        if new_location.nil? || new_location == starting_point
+          bad_locations[previous_location.last].push(current_location)
+          current_location = previous_location.pop
+        elsif previous_location.include?(new_location)
+          puts "Cyclical?" if previous_location.last != new_location
+          bad_locations[current_location].push(new_location)
+        else
+          previous_location.push(current_location)
+          current_location = new_location
+        end
+      else
+        previous_location.push(current_location)
+        current_location = new_location
+      end
+      iteration += 1
+    end
+end
+
+drive_page('http://jobs.happier.com/1111')
